b2da31486c487cb8b58107c895783f2ecd5e6aef
ginger59620 <a59620@alunos.ipb.pt>
2025-06-03 19:12:19 +0100

    Trying to make the keybinds

---
+++ Assets/Scripts/Keybinds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keybinds : MonoBehaviour
+{
+   // private Dictionary<string, KeyCode> Keys = new Dictionary<string, KeyCode>();
+
+    public TextMeshPro Up, Left, Down, Right;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       /* Keys.Add("Up", KeyCode.K);
+        keys.Add("Left", KeyCode.A);
+        keys.Add("Down", KeyCode.S);
+        keys.Add("Right", KeyCode.L);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
