5f953e794eafeb92afbb45c37cf4b049b18b2908
ginger59620 <a59620@alunos.ipb.pt>
2025-06-19 15:06:45 +0100

    music menu

--- Assets/Scripts/LevelSelectiong.cs
+++ Assets/Scripts/LevelSelectiong.cs
@@ -7,10 +7,12 @@ public class LevelSelectiong : MonoBehaviour
     [SerializeField] private Button nextButton;
 
     private int currentLevel;
+    private int currentSong;
 
     private void Awake()
     {
         SelectLevel(0);
+        SelectMusic(0);
     }
 
     private void SelectLevel(int _index)
@@ -28,5 +30,22 @@ public class LevelSelectiong : MonoBehaviour
     {
         currentLevel += _change;
         SelectLevel(currentLevel);
+    }
+
+    private void SelectMusic(int _index)
+    {
+        lastButton.interactable = (_index != 0); //makes the last button dissable
+        nextButton.interactable = (_index != transform.childCount - 1);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == _index);
+        }
+    }
+
+    public void ChangeMusic(int _change)
+    {
+        currentSong += _change;
+        SelectMusic(currentSong);
     }
 }
